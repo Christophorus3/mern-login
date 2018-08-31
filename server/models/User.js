b71aaedb0b6522e18f67dccdaeb265c3942f2a39
christophwottawa <christoph.wottawa@me.com>
2018-08-31 09:44:29 +0200

    Added login, verify, logout routes to server

--- server/models/User.js
+++ server/models/User.js
@@ -24,11 +24,11 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.methods.generateHash = (password) => {
+UserSchema.methods.generateHash = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8, null))
 };
 
-UserSchema.methods.validPassword = (password) => {
+UserSchema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.password)
 };
 
--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -1,4 +1,5 @@
 const User = require('../../models/User');
+const UserSession = require('../../models/UserSession');
 
 module.exports = (app) => {
   /*
@@ -12,7 +13,9 @@ module.exports = (app) => {
       email,
       password
     } = body;
-    
+
+    email = email.toLowerCase();
+
     if (!firstName) {
       return res.send({
         success: false,
@@ -40,8 +43,6 @@ module.exports = (app) => {
         message: 'Error: password can not be blank.'
       })
     }
-
-    email = email.toLowerCase();
 
     User.find({
       email: email
@@ -77,5 +78,130 @@ module.exports = (app) => {
         })
       })
     })
-  })
+  });
+
+  app.post('/api/account/login', (req, res, next) => {
+    const {body} = req;
+    let {
+      email,
+      password
+    } = body;
+
+    email = email.toLowerCase();
+
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: email can not be blank.'
+      })
+    }
+
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: password can not be blank.'
+      })
+    }
+
+    User.find({
+      email: email
+    }, (err, users) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: "Error: server error."
+        });
+      }
+      if (users.length != 1) {
+        return res.send({
+          success: false,
+          message: "Error: Invalid user email."
+        });
+      }
+
+      let user = users[0];
+
+      console.log(user);
+
+      if (!user.validPassword(password)) {
+        return res.send({
+          success: false,
+          message: "Error: Invalid password."
+        });
+      }
+
+      let userSession = new UserSession();
+      userSession.userId = user._id;
+      userSession.save((err, session) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: "Error: server error."
+          });
+        }
+
+        return res.send({
+          success: true,
+          message: "User login valid!",
+          token: session._id
+        })
+      })
+    })
+  });
+
+  app.get('/api/account/verify', (req, res, next) => {
+    const { query } = req;
+    const { token } = query;
+
+    console.log(token);
+
+    UserSession.find({
+      _id: token,
+      isDeleted: false
+    }, (err, sessions) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: "Error: Server error."
+        })
+      }
+      console.log(sessions);
+
+      if (sessions.length != 1) {
+        return res.send({
+          success: false,
+          message: "Error: No valid session."
+        })
+      } else {
+        return res.send({
+          success: true,
+          message: "Success: This session is valid."
+        })
+      }
+    })
+  });
+
+  app.get('/api/account/logout', (req, res, next) => {
+    const { query } = req;
+    const { token } = query;
+
+    UserSession.findOneAndUpdate({
+      _id: token,
+      isDeleted: false
+    }, {
+      $set: {isDeleted: true}
+    }, null, (err, sessions) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: "Error: Server error."
+        })
+      }
+
+      return res.send({
+        success: true,
+        message: "Success: This session is ended. User logged out."
+      })
+    })
+  });
 };
